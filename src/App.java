68e0c6be8db760b20262ca78b8529fa83d053d2a
SilverSun000 <williamcahill78@gmail.com>
2025-08-30 17:28:42 -0500

    Finally got Junit working on Arch

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public int modulus(int a, int b) {
+      return a % b;
+    }
+
+    public static void main(String[] args) {
+      int x = 5, y = 15;
+      App finder = new App();
+      System.out.println(finder.modulus(y, x));
     }
 }
--- src/AppTest.java
+++ src/AppTest.java
@@ -1,11 +1,30 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 
 public class AppTest {
 
+    App app;
+
+    @BeforeEach
+    void setup() {
+      app = new App();
+    }
+
     @Test
-    public void test() {
-        assertEquals(2, 1 + 1);;
+    @DisplayName("Simple Modulus should work")
+    void testModulus() {
+        assertEquals(0, app.modulus(15, 5),
+          "Regular Modulus sould work");
+    }
+
+    @RepeatedTest(5)
+    @DisplayName("Ensure that we can receive non zero answers as well")
+    void testModulusNonZeroReturn() {
+      assertEquals(1, app.modulus(5, 4), "Four modulus five should be 1");
+      assertEquals(2, app.modulus(5, 3), "Three modulus 5 should be 2");
     }
 }
